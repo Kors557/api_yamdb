72d4a2abd551a0697d9ad3610a9b0da1164cb7c7
iKors <1500459@mail.ru>
2022-05-10 20:41:22 +0300

    работа над users и добавлено приложение api

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,7 +3,20 @@ from django.db import models
 
 
 class User(AbstractUser):
-    # role = ?
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+    ROLE = [
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin')
+    ]
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE,
+        default=USER,
+    )
     username = models.CharField(
         "Имя пользователя",
         max_length=150,
@@ -11,12 +24,20 @@ class User(AbstractUser):
         blank=False,
         null=False,
     )
-    bio = models.TextField('Биография', blank=True,)
+    bio = models.TextField("Биография", blank=True,)
     email = models.EmailField("email", unique=True, null=False, max_length=254)
     first_name = models.CharField("Имя", max_length=150, blank=True)
     last_name = models.CharField("Фамилия", max_length=150, blank=True)
-    confirmation_code = models.CharField(
-        'Код подтверждения',
-        max_length=155,
-        null=True
-    )
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN or self.is_superuser
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
+    
